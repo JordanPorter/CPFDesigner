bf3741483c2c33a1e8e16f830b05a70cf748e826
jorporte <jordan.j.porter@gmail.com>
2016-04-04 11:10:45 -0230

    Begin Database Programming.
    Able to retrieve some info from HTML via JSON
    Need to add more data to transfer and then add to db.

--- Outcome_Creator/controllers/outcome_controller.js
+++ Outcome_Creator/controllers/outcome_controller.js
@@ -1,38 +1,69 @@
-app.controller('outcome_controller', ['$scope', function($scope)	{
-	$scope.test = 'TEST',
+app.controller('outcome_controller', ['$scope', '$http', function($scope, $http)	{
+	
+	$scope.provinces = [
+	        {
+	        	id: 'NL',
+	        	name: 'Newfoundland and Labrador',
+	        }
+	    ],
 	$scope.courses = [
-	    {
-			name: 'Mathematics'
-		},
-		{
-			name: 'Science'
-		},
-		{
-			name: 'Social Studies'
-		},
-		{
-			name: 'Health'
-		},
-		{
-			name: 'Religious Education'
-		},
-		{
-			name: 'Health'
-		},
-		{
-			name: 'Technology'
-		},
-		{
-			name: 'Language Arts'
-		},
-		{
-			name: 'Art'
-		},
-		{
-			name: 'Music'
-		},
-		{
-			name: 'Physical Education'
-		}
-	]
+		    {
+		    	id: 'MA',
+				name: 'Mathematics'
+			},
+			{
+				id: 'SC',
+				name: 'Science'
+			},
+			{
+				id: 'SS',
+				name: 'Social Studies'
+			},
+			{
+				id: 'H',
+				name: 'Health'
+			},
+			{
+				id: 'RE',
+				name: 'Religious Education'
+			},
+			{
+				id: 'T',
+				name: 'Technology'
+			},
+			{
+				id: 'LA',
+				name: 'Language Arts'
+			},
+			{
+				id: 'A',
+				name: 'Art'
+			},
+			{
+				id: 'MU',
+				name: 'Music'
+			},
+			{
+				id: 'PE',
+				name: 'Physical Education'
+			}
+		],
+	$scope.add = function()	{
+		//why is it not creating that object? but thats teh way to do it, on the other side you can do req.body.province, etc.
+		//ahh i see... well the .value property isnt defined for angular stuff as far as I can tell. I've never been able to use .value
+		//okay lets see
+		// this is weird, is that how you had it?
+		// uhhhhhhhhhhhh........ nope. i just mean to get the data, not how you had it in an array
+		// OOH yes that the wasy
+		//the wasy...  its working as intended now, just need to get the select values correctly.
+		
+		var info = {
+				province: $scope.province_select,
+				course: $scope.course_select
+		};
+		console.log(info);
+		$http.post('./add', JSON.stringify(info)).success(function(data, status){
+			console.log('Post Requested');
+		});
+	}
 }]);
--- Outcome_Creator/server/outcome_server.js
+++ Outcome_Creator/server/outcome_server.js
@@ -2,7 +2,10 @@
  * 
  */
 
+var http = require('http');
+var fs = require('fs');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 const PORT = 8080;
 
@@ -12,53 +15,82 @@ var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/wiseowl';
 
-var insert = function(db, callback)	{
-	db.collection('provinces').insertOne({_id : 01, "name" : "nl", "city" : "stj"}, function(err, result)	{
-		assert.equal(null, err);
-		console.log("Inserted Document");
-		callback();
-	});
-};
+//var insert = function(db, callback)	{
+//	db.collection('provinces').insertOne({_id : 01, "name" : "nl", "city" : "stj"}, function(err, result)	{
+//		assert.equal(null, err);
+//		console.log("Inserted Document");
+//		callback();
+//	});
+//};
+//
+//var find = function(db, callback)	{
+//	var curser = db.collection('provinces').find();
+//	curser.each(function(err, doc)	{
+//		assert.equal(null, err);
+//		if(doc != null)	{
+//			console.dir(doc);
+//		} else	{
+//			callback();
+//		}
+//	});
+//};
+//
+//var update = function(db, callback)	{
+//	db.collection('provinces').updateOne({ "name" : "nl" }, { $set: {"city" : "to"}}, function(err, results)	{
+//		callback();
+//	});
+//};
+//
+//var remove = function(db, callback)	{
+//	db.collection('provinces').deleteMany({"name" : "nl"}, function(err, results)	{
+//		console.log(results);
+//		callback();
+//	});
+//};
+//
+//MongoClient.connect(url, function(err, db)	{
+//	assert.equal(null, err);
+//	find(db, function()	{
+//		db.close();
+//	});
+//});
 
-var find = function(db, callback)	{
-	var curser = db.collection('provinces').find();
-	curser.each(function(err, doc)	{
-		assert.equal(null, err);
-		if(doc != null)	{
-			console.dir(doc);
-		} else	{
-			callback();
-		}
-	});
-};
-
-var update = function(db, callback)	{
-	db.collection('provinces').updateOne({ "name" : "nl" }, { $set: {"city" : "to"}}, function(err, results)	{
-		callback();
-	});
-};
-
-var remove = function(db, callback)	{
-	db.collection('provinces').deleteMany({"name" : "nl"}, function(err, results)	{
-		console.log(results);
-		callback();
-	});
-};
-
-MongoClient.connect(url, function(err, db)	{
-	assert.equal(null, err);
-	find(db, function()	{
-		db.close();
-	});
-});
-
-app.use('/apps', express.static(__dirname + '/apps'));
-app.use('controllers', express.static(__dirname + '/controllers'));
-
-
+app.use('/apps', express.static('../apps'));
+app.use('/controllers', express.static('../controllers'));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use( bodyParser.json() );
 
 app.get('/', function(req, res)	{
 	res.sendFile("/Users/jorporte/Documents/HTML5/Curriculum Planning Framework - Drag n' Drop/Outcome_Creator/index.html");
+});
+
+app.post('/add', function(req, res){
+	console.log("Add Executed:");
+	console.log(req.body);
+	res.sendStatus(200);
+	console.log(req.body.province);
+	
+//	var insert = function(db, callback) {
+//		db.collection('provinces').insertOne({
+//			_id : 01,
+//			"name" : "nl",
+//			"city" : "stj"
+//		}, function(err, result) {
+//			assert.equal(null, err);
+//			console.log("Inserted Document");
+//			callback();
+//		});
+//	};
+//	
+//	MongoClient.connect(url, function(err, db) {
+//		assert.equal(null, err);
+//		find(db, function() {
+//			db.close();
+//		});
+//	});
+
 });
 
 app.listen(PORT, function()	{
